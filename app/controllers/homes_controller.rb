0907b42ea428cf10e799f28bf582d2550480afd8
Elan Baharie and Jesse Hoare Evans <elan+jesse@>
2017-07-13 16:46:35 -0700

    final commit of the week

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,20 +1,14 @@
 class HomesController < ApplicationController
 
   def index
-    
   end
 
   def test
-
     call_info = params['testy']
-
-
     if call_info
-      p call_info['zip']
       zip_from_form = call_info['zip']
       weather_object = Weather.new(zip_from_form)
       @humidity = weather_object.get_humidity()
     end
-
   end
 end
